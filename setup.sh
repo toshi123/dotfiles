8fbca86c72ae9baa54409a9e0dff351875152255
toshi123 <toshibp@gmail.com>
2012-10-18 15:12:48 +0900

    setup.shを修正

--- setup.sh
+++ setup.sh
@@ -4,9 +4,16 @@ DOT_FILES=( .zshrc .emacs .emacs.d )
 
 for file in ${DOT_FILES[@]}
 do
-    if[-a $HOME/$file]; then
-        echo "file already exists: $file"
+    if [ -a $HOME/$file ]; then
+        if [ -L $HOME/$file ]; then
+            echo "既にシンボリックリンクが存在します: $file"
+        elif [ -d $HOME/$file ]; then
+            echo "既にディレクトリが存在します: $file"
+        else
+            echo "既にファイルが存在します: $file"
+        fi
     else
         ln -s $HOME/dotfiles/$file $HOME/$file
+        echo "シンボリックリンクを貼りました: $file"
     fi
 done
